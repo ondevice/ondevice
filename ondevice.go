cef85bd31e0ee5ae3d62f4ec089350b3b4dabeb2
Manuel Reithuber <git@manuel.reithuber.net>
2017-01-17 22:21:21 +0100

    ondevice.go: added len(argv) check

--- ondevice.go
+++ ondevice.go
@@ -1,11 +1,18 @@
 package main
 
-import "github.com/ondevice/ondevice-cli/command"
+import (
+	"log"
+	"os"
 
-import "os"
+	"github.com/ondevice/ondevice-cli/command"
+)
 
 func main() {
-	cmd := os.Args[1]
+	if len(os.Args) < 2 {
+		log.Fatal("Missing command! try 'ondevice help'")
+	}
 
+	//log.Print("-- args: ", os.Args[1:])
+	cmd := os.Args[1]
 	command.Run(cmd, os.Args[2:])
 }

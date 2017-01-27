ab777d16b3df371db8d19ecc6297efb46fecf238
Manuel Reithuber <git@manuel.reithuber.net>
2017-01-27 15:40:27 +0100

    added config.GetDeviceID()

--- config/device.go
+++ config/device.go
@@ -1,5 +1,14 @@
 package config
 
+// GetDeviceID -- Returns the devId if available (otherwise returns an empty string)
+func GetDeviceID() string {
+	rc, err := GetValue("device", "dev-id")
+	if err != nil {
+		return ""
+	}
+	return rc
+}
+
 // GetDeviceKey -- Returns the device's key (or an empty string if not defined)
 func GetDeviceKey() string {
 	rc, err := GetValue("device", "key")

64d825e256b2c4f62e63443a07913c5a0c2b99eb
Manuel Reithuber <git@manuel.reithuber.net>
2017-01-28 21:10:02 +0100

    added config.setFilePath()

--- config/config.go
+++ config/config.go
@@ -12,12 +12,21 @@ import "path"
 // if not nil, this will be used instead of `~/.config/ondevice` (mainly used for testing)
 var _configPathOverride *string
 
+var _fileOverrides = map[string]string{}
+
 func setConfigPath(path string) {
 	_configPathOverride = &path
 }
 
 // GetConfigPath -- Return the full path of a file in our config directory (usually ~/.config/ondevice/)
+// Can be overridden using setConfigPath() (for testing only) or SetFilePath()
 func GetConfigPath(filename string) string {
+	if _, ok := _fileOverrides[filename]; ok {
+		return _fileOverrides[filename]
+	}
+
+	// global config path override (used in unit tests)
+	// TODO replace with single file overrides
 	if _configPathOverride != nil {
 		return path.Join(*_configPathOverride, filename)
 	}
@@ -55,6 +64,11 @@ func GetValue(section string, key string) (string, error) {
 	}
 
 	return val.String(), nil
+}
+
+// SetFilePath -- Override a config file's path (e.g. to satisfy standard OS paths)
+func SetFilePath(filename string, path string) {
+	_fileOverrides[filename] = path
 }
 
 // SetValue -- create/update a config value

6fcffb614e2ce9ea8cc21952295860e4e2d81bbc
Manuel Reithuber <git@manuel.reithuber.net>
2018-01-28 15:37:18 +0100

    ondevice stop: exit code now distinguishes between timeout/error and 'wasn't running in the first place'

--- command/stop.go
+++ command/stop.go
@@ -10,13 +10,13 @@ import (
 
 func runStop(args []string) int {
 	var found = false
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 30; i++ {
 		// fetch the PID every time (the daemon might go inactive - still running until the remaining tunnels close)
 		p, err := daemon.GetDaemonProcess()
 		if err != nil {
 			if !found {
 				logg.Debug("GetDaemonProcess error: ", err)
-				logg.Fatal("Couldn't find daemon process")
+				logg.Error("Couldn't find daemon process")
 				return 1
 			}
 			// we seem to have stopped it
@@ -32,8 +32,8 @@ func runStop(args []string) int {
 		time.Sleep(1000 * time.Millisecond)
 	}
 
-	logg.Fatal("Timeout trying to stop the ondevice daemon")
-	return 1
+	logg.Error("Timeout trying to stop the ondevice daemon")
+	return 2
 }
 
 // StopCommand -- Stops the ondevice daemon (by sending SIGTERM)
@@ -41,9 +41,11 @@ var StopCommand = BaseCommand{
 	Arguments: "",
 	LongHelp: `$ ondevice stop
 
-Stops a running ondevice daemon (using the ondevice.pid file) and tries to terminate it.
+Stops a running ondevice daemon (using the ondevice.pid file) and tries to
+terminate it.
 
-Returns 0 on success or 1 on error
+Returns 0 if the daemon was stopped, 1 if it wasn't running and 2 on error
+(e.g. after a 30sec timeout)
 `,
 	ShortHelp: "Stops the local ondevice daemon (if running)",
 	RunFn:     runStop,

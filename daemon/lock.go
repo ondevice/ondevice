8bb0a0fac4d008956fcaf8aae1ae34326cd04681
Manuel Reithuber <git@manuel.reithuber.net>
2018-01-28 13:52:43 +0100

    daemon.fileLock: some minor improvements (should fix certain race conditions)

--- daemon/lock.go
+++ daemon/lock.go
@@ -29,14 +29,16 @@ func (l *lockFile) TryLock() error {
 	if l.fd, err = syscall.Open(l.Path, os.O_CREATE|os.O_WRONLY, 0644); err != nil {
 		return fmt.Errorf("Couldn't open '%s' for locking: %s", l.Path, err)
 	}
-	l.closed = false
 
 	if err = syscall.Flock(l.fd, syscall.LOCK_EX|syscall.LOCK_NB); err != nil {
+		syscall.Close(l.fd)
 		return fmt.Errorf("ondevice daemon seems to be running already (%s)", err)
 	}
+
+	l.closed = false
 	logg.Debug("acquired daemon lock file")
 
-	// only do this if we've got the lock
+	// only do this once we've got the lock
 	logg.Debug("Writing to PID file: ", os.Getpid())
 	pidstr := fmt.Sprintf("%d\n", os.Getpid())
 	syscall.Write(l.fd, []byte(pidstr))
@@ -51,16 +53,17 @@ func (l *lockFile) Unlock() error {
 		return nil
 	}
 
-	if err = syscall.Flock(l.fd, syscall.LOCK_UN); err != nil {
-		return fmt.Errorf("Failed to unlock lock file: %s", err)
-	}
-	if err = syscall.Close(l.fd); err != nil {
-		return fmt.Errorf("Failed to close lock file: %s", err)
-	}
+	// Note: we're not doing close(), then delete() because that could lead to
+	// race conditions (we close, another process acquires lock, we unlink the
+	// file, rendering their lock ineffective)
 
-	// remove PID file
+	// remove PID file (rendering the (still active) lock inactive)
 	if err = os.Remove(l.Path); err != nil {
 		return fmt.Errorf("Failed to remove PID file: %s", err)
+	}
+
+	if err = syscall.Close(l.fd); err != nil {
+		return fmt.Errorf("Failed to close lock file: %s", err)
 	}
 
 	l.closed = true

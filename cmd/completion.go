3e2762df22356dc7612b8cb4ba9e53f32a7458f1
Manuel Reithuber <git@manuel.reithuber.net>
2020-05-25 19:40:01 +0200

    cmd/completion.go: removed cobra comment

--- cmd/completion.go
+++ cmd/completion.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+Copyright © 2020 ondevice.io <dev@ondevice.io>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -42,14 +42,4 @@ To configure your bash shell to load completions for each session add to your ba
 
 func init() {
 	rootCmd.AddCommand(completionCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// completionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// completionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }

cbf9d258de42a36133ee5dd3425ea5271caff61b
Manuel Reithuber <git@manuel.reithuber.net>
2020-05-26 16:23:36 +0200

    ondevice help list: fixed formatting

--- cmd/list.go
+++ cmd/list.go
@@ -43,58 +43,53 @@ func init() {
 	c.Command = cobra.Command{
 		Use:   "list [filters...]",
 		Short: "list your devices",
-		Long: `List your devices
+		Long: `list your devices, optionally filtering the results
 
-			Filters:
+Filters:
+  with filters you can limit the output based on device properties.
 
-			With filters you can limit the output based on device properties.
+  filters have the format:
+    propertyName[operator[value]]
+  e.g.:
+    somePackageVersion<2.3.4
+    foo=
 
-			Filters have the format:
-				propertyName[operator[value]]
-			e.g.:
-				somePackageVersion<2.3.4
-				foo=
+  All comparisons are string-based (e.g. "234" is less than "99")
+  Missing values are treated like the empty string
+  ("arch=" lists devices with empty or missing 'arch' property).
 
-			All comparisons are string-based (e.g. "234" is less than "99")
-			Missing properties are treated like the empty string ("arch=" lists devices with
-			empty or missing 'arch' property).
+  The following comparison operators are supported (comma-separated):
+    "=,==,!=,<,<<,<=,>,>>,>="
 
+  The one-character operators ("=,<,>") each have a two-character alias ("==,<<,>>"
+  respectively). These are provided to fix ambiguities (if the value to c start with "")
+  Note that you might have to escape '>' and '<' to shell redirection`,
+		Example: `
+  $ ondevice list
+  ID            State   IP             Version         Name
+  demo.7t91ta   offline                ondevice v0.4.3
+  demo.fbqh2p   offline 192.168.1.23   ondevice v0.3.9
+  demo.q5dkpm   online  127.0.0.1      ondevice v0.4.2
+  demo.thm7br   offline 10.0.0.127     ondevice v0.4.3 My Raspberry PI
 
-			The following comparison operators are supported (comma-separated):
-			"=,==,!=,<,<<,<=,>,>>,>="
+#Example: using filters
+  $ ondevice list 'fooVersion<2.3.4' 'foo=' --print-ids
+  demo.7t91ta
+  demo.fbqh2p
+  demo.thm7br
 
-			The one-character operators ("=,<,>") each have a two-character alias ("==,<<,>>"
-			respectively). These are provided to fix ambiguities (if the value to c start with "")
-			Note that you might have to escape '>' and '<' to shell redirection
+  #only lists devices with the "fooVersion" property less than "2.3.4" (simple
+  #string comparison, so "2.3.4" < "2.34.5") and without the "foo" property
+  #(unset is equivalent to "")
 
+#Example: JSON output
+  $ ondevice list --json --props
+  {"id":"demo.7t91ta",state":"offline","stateTs":1490197318991,"version":"ondevice v0.4.3"}
+  {"id":"demo.fbqh2p","ip":"192.168.1.23","state":"offline","stateTs":1485721709598,"version":"ondevice v0.3.9"}
+  {"id":"demo.q5dkpm","ip":"127.0.0.1","state":"offline","stateTs":1487068641353,"version":"ondevice v0.4.2","props":{"test":"1234"}}
+  {"id":"demo.thm7br","ip":"10.0.0.127","state":"offline", "": "My Raspberry PI","stateTs":1490963689912,"version":"ondevice v0.4.3"}
 
-			Example:
-				$ ondevice list
-				ID            State   IP             Version         Name
-				demo.7t91ta   offline                ondevice v0.4.3
-				demo.fbqh2p   offline 192.168.1.23   ondevice v0.3.9
-				demo.q5dkpm   online  127.0.0.1      ondevice v0.4.2
-				demo.thm7br   offline 10.0.0.127     ondevice v0.4.3 My Raspberry PI
-
-			Example: Filters
-				$ ondevice list 'fooVersion<2.3.4' 'foo=' --print-ids
-				demo.7t91ta
-				demo.fbqh2p
-				demo.thm7br
-
-				Only lists devices with the "fooVersion" property less than "2.3.4" (simple
-				string comparison, so "2.3.4" < "2.34.5") and without the "foo" property
-				(unset is equivalent to "")
-
-			Example: JSON output
-				$ ondevice list --json --props
-				{"id":"demo.7t91ta",state":"offline","stateTs":1490197318991,"version":"ondevice v0.4.3"}
-				{"id":"demo.fbqh2p","ip":"192.168.1.23","state":"offline","stateTs":1485721709598,"version":"ondevice v0.3.9"}
-				{"id":"demo.q5dkpm","ip":"127.0.0.1","state":"offline","stateTs":1487068641353,"version":"ondevice v0.4.2","props":{"test":"1234"}}
-				{"id":"demo.thm7br","ip":"10.0.0.127","state":"offline", "": "My Raspberry PI","stateTs":1490963689912,"version":"ondevice v0.4.3"}
-
-				Note that JSON fields may be missing or null
-		`,
+  #note that JSON fields may be missing or null`,
 		Run: c.run,
 	}
 	rootCmd.AddCommand(&c.Command)

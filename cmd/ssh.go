8170259889c729d8cc9219fee447b59e5ad7d038
Manuel Reithuber <git@manuel.reithuber.net>
2020-05-26 19:48:54 +0200

    ondevice help ssh: fixed formatting

--- cmd/ssh.go
+++ cmd/ssh.go
@@ -44,33 +44,34 @@ func init() {
 		Short: "connect to your devices using the ssh protocol",
 		Long: `Connect to your devices using the 'ssh' command.
 
-		This is a relatively thin wrapper around the 'ssh' command.
-		The main difference to invoking ssh directly is that instead of regular host names you'll have to specify an ondevice deviceId.
-		The connection is routed through the ondevice.io network.
+This is a relatively thin wrapper around the 'ssh' command.
+The main difference to invoking ssh directly is that instead of regular host names you'll have to specify an ondevice deviceId.
+The connection is routed through the ondevice.io network.
 
-		ondevice ssh will try to parse ssh's arguments, the first non-argument has to be
-		the user@devId combo.
+ondevice ssh will try to parse ssh's arguments, the first non-argument has to be
+the user@devId combo.
 
-		See ssh's documentation for further details.
+See ssh's documentation for further details.
 
-		Examples:
-		- ondevice ssh device1
-		  simply connect to device1
-		- ondevice ssh user@device1
-		  open an SSH connection to device1, logging in as 'user'
-		- ondevice ssh device1 echo hello world
-		  run 'echo hello world' on device1
-		- ondevice ssh device1 -N -L 1234:localhost:80
-		  Tunnel the HTTP server on device1 to the local port 1234 without opening
-		  a shell
-		- ondevice ssh device1 -D 1080
-		  Starting a SOCKS5 proxy listening on port 1080. It'll redirect all traffic
-		  to the target host.
+Notes:
+- We use our own known_hosts file (in ~/.config/ondevice/known_hosts).
+  Override with ''-oUserKnownHostsFile=...'`,
+		Example: `- simply connect to device1:
+  $ ondevice ssh device1
 
-		Notes:
-		- We use our own known_hosts file (in ~/.config/ondevice/known_hosts).
-		  Override with ''-oUserKnownHostsFile=...'
-	`,
+- open an SSH connection to device1, logging in as 'user':
+  $ ondevice ssh user@device1
+
+- run 'echo hello world' on device1:
+  $ ondevice ssh device1 echo hello world
+
+- tunnel the HTTP server on device1 to the local port 1234 without opening
+  a shell:
+  $ ondevice ssh device1 -N -L 1234:localhost:80
+
+- start a SOCKS5 proxy listening on port 1080. It'll redirect all traffic
+  to the target host:
+  $ ondevice ssh device1 -D 1080`,
 		Run:               c.run,
 		ValidArgsFunction: internal.DeviceListCompletion{}.Run,
 	}

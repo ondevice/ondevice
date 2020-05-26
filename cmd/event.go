dcedc6cf2938a9964551bc4a44024d2bc54b6bee
Manuel Reithuber <git@manuel.reithuber.net>
2020-05-26 14:17:13 +0200

    cmd/event.go: fixed linter warnings

--- cmd/event.go
+++ cmd/event.go
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package cmd
 
 import (
@@ -180,7 +181,7 @@ func (c *eventCmd) onEvent(ev api.Event) error {
 	return nil
 }
 
-func (e *eventCmd) onTimeout() {
+func (*eventCmd) onTimeout() {
 	// TODO exit gracefully (closing the listener etc.)
 	logrus.Info("event stream timeout")
 	os.Exit(2)

8d589f8e9103e39720aad3406267fcf3b13f5047
Manuel Reithuber <git@manuel.reithuber.net>
2020-05-31 18:45:04 +0200

    added config.Auth.WithAPIServer()

--- config/internal/auth.go
+++ config/internal/auth.go
@@ -2,13 +2,16 @@ package internal
 
 // Auth -- contains immutable API server credentials
 type Auth interface {
-	// User name
+	// User -- user name
 	User() string
-	// API key for the given user
+	// Key -- API key for the given user
 	Key() string
 
-	// API server these credentials are valid for - if "", defaults to https://api.ondevice.io/
+	// APIServer -- API server these credentials are valid for - if "", defaults to https://api.ondevice.io/
 	APIServer() string
+
+	// WithAPIServer -- returns a copy of this object, but with the given API server
+	WithAPIServer(newServer string) Auth
 }
 
 // AuthEntry -- implements the Auth interface
@@ -26,6 +29,15 @@ func (a AuthEntry) Key() string { return a.KeyField }
 
 // APIServer -- returns the API server URL
 func (a AuthEntry) APIServer() string { return a.APIServerField }
+
+// WithAPIServer -- returns a modified copy
+func (a AuthEntry) WithAPIServer(newServer string) Auth {
+	return AuthEntry{
+		UserField:      a.UserField,
+		KeyField:       a.KeyField,
+		APIServerField: newServer,
+	}
+}
 
 // NewAuth -- creates Auth credentials
 func NewAuth(username, key, apiServer string) Auth {

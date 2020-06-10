ce2d0ed8c3724880bec41105a9548b5b703f76d8
Manuel Reithuber <git@manuel.reithuber.net>
2020-06-10 14:52:33 +0200

    ondevice config set: added '--no-overwrite' flag, moved configSetCmd to its own file

--- cmd/config.go
+++ cmd/config.go
@@ -20,7 +20,6 @@ import (
 	"fmt"
 	"os"
 	"sort"
-	"strings"
 
 	"github.com/ondevice/ondevice/cmd/internal"
 	"github.com/ondevice/ondevice/config"
@@ -96,48 +95,6 @@ when only one key is requested, only the value will be printed`,
 	ValidArgsFunction: internal.ConfigCompletion{WithReadOnly: true}.Run,
 }
 
-var configSetCmd = &cobra.Command{
-	Use:   "set <key>=<value>...",
-	Short: "set one or more configuration values",
-	Long: `ondevice config set updates one or more configuration values.
-If you specify the same key more than once, the last one in the list wins`,
-	Example: `  $ ondevice config set ssh.path=/usr/local/bin/ssh rsync.path=echo
-  $ ondevice config set client.timeout=5`,
-	Run: func(cmd *cobra.Command, args []string) {
-		var rc = 0
-
-		var cfg = config.MustLoad()
-		for _, keyValue := range args {
-			var parts = strings.SplitN(keyValue, "=", 2)
-			if len(parts) != 2 {
-				logrus.Fatalf("malformed argument, expected key=value pairs: '%s'", keyValue)
-			}
-			var key = config.FindKey(parts[0])
-			var newValue = parts[1]
-
-			if key == nil {
-				logrus.Fatalf("config key not found: '%s'", keyValue)
-				rc = 1
-				continue
-			}
-
-			if err := cfg.SetValue(*key, newValue); err != nil {
-				rc = 1
-			}
-		}
-
-		if rc != 0 {
-			os.Exit(rc)
-		}
-
-		if err := cfg.Write(); err != nil {
-			logrus.WithError(err).Error("failed to write config")
-		}
-	},
-	Args:              cobra.MinimumNArgs(1),
-	ValidArgsFunction: internal.ConfigCompletion{WithReadOnly: false, Suffix: "="}.Run,
-}
-
 var configUnsetCmd = &cobra.Command{
 	Use:   "unset <key>...",
 	Short: "revert one or more configuration values to their defaults",
@@ -175,6 +132,5 @@ var configUnsetCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(configCmd)
 	configCmd.AddCommand(configGetCmd)
-	configCmd.AddCommand(configSetCmd)
 	configCmd.AddCommand(configUnsetCmd)
 }
---
+++ cmd/config_set.go
@@ -0,0 +1,78 @@
+package cmd
+
+import (
+	"os"
+	"strings"
+
+	"github.com/ondevice/ondevice/cmd/internal"
+	"github.com/ondevice/ondevice/config"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	var c configSetCmd
+	c.Command = cobra.Command{
+		Use:   "set <key>=<value>...",
+		Short: "set one or more configuration values",
+		Long: `ondevice config set updates one or more configuration values.
+	If you specify the same key more than once, the last one in the list wins`,
+		Example: `  $ ondevice config set ssh.path=/usr/local/bin/ssh rsync.path=echo
+	  $ ondevice config set client.timeout=5`,
+		Run:               c.Run,
+		Args:              cobra.MinimumNArgs(1),
+		ValidArgsFunction: internal.ConfigCompletion{WithReadOnly: false, Suffix: "="}.Run,
+	}
+
+	configCmd.AddCommand(&c.Command)
+	c.Flags().BoolVar(&c.noOverwriteFlag, "no-overwrite", false, "if set, won't overwrite an already existing value (i.e. only set if not yet defined')")
+
+}
+
+type configSetCmd struct {
+	cobra.Command
+
+	noOverwriteFlag bool
+}
+
+func (c *configSetCmd) Run(cmd *cobra.Command, args []string) {
+	var rc = 0
+
+	var cfg = config.MustLoad()
+	for _, keyValue := range args {
+		var parts = strings.SplitN(keyValue, "=", 2)
+		if len(parts) != 2 {
+			logrus.Fatalf("malformed argument, expected key=value pairs: '%s'", keyValue)
+		}
+		var key = config.FindKey(parts[0])
+		var newValue = parts[1]
+
+		if key == nil {
+			logrus.Fatalf("config key not found: '%s'", keyValue)
+			rc = 1
+			continue
+		}
+
+		if c.noOverwriteFlag {
+			if err := cfg.SetNX(*key, newValue); err != nil {
+				rc = 1
+			}
+		} else {
+			if err := cfg.SetValue(*key, newValue); err != nil {
+				rc = 1
+			}
+		}
+	}
+
+	if rc != 0 {
+		os.Exit(rc)
+	}
+	if !cfg.IsChanged() {
+		logrus.Info("ondevice set: nothing changed")
+		return
+	}
+
+	if err := cfg.Write(); err != nil {
+		logrus.WithError(err).Error("failed to write config")
+	}
+}

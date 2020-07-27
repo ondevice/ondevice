4e268342f83b6ecbc4c4de8c590a77accc0637aa
Manuel Reithuber <git@manuel.reithuber.net>
2020-07-27 20:18:16 +0200

    updated pathvalidator_test to actually reflect the behaviour we want

--- config/internal/pathvalidator_test.go
+++ config/internal/pathvalidator_test.go
@@ -45,7 +45,6 @@ func TestValidateSinglePathWithURLs(t *testing.T) {
 		ValidSchemes: map[string]bool{"": true, "unix": true, "http": true},
 	}
 
-	assert.NoError(validator.Validate(""))
 	var value = validator.Value("")
 	assert.NoError(value.Error)
 	assert.Empty(value.values)
@@ -54,25 +53,22 @@ func TestValidateSinglePathWithURLs(t *testing.T) {
 
 	// single valid URL
 	var str = "http://localhost:8080/"
-	assert.NoError(validator.Validate(str))
 	value = validator.Value(str)
 	assert.NoError(value.Error)
 	assert.Len(value.values, 1)
 	assert.False(value.HasNext())
 	assert.Equal(str, value.String("--defaultValue--"))
 
-	// single URL with invalid schema
+	// single URL with invalid schema (.Value will still return the stored value even if it's technically invalid)
 	str = "https://ondevice.io/index.html"
-	assert.Error(validator.Validate(str))
 	value = validator.Value(str)
 	assert.Error(value.Error)
 	assert.Len(value.values, 1)
 	assert.False(value.HasNext())
 	assert.Equal(str, value.String("--defaultValue--"))
 
-	// JSON values will cause validation errors
+	// JSON values will cause URL validation errors
 	str = "[\"https://ondevice.io/index.html\"]"
-	assert.Error(validator.Validate(str))
 	value = validator.Value(str)
 	assert.Error(value.Error)
 	assert.Len(value.values, 1)
@@ -81,20 +77,95 @@ func TestValidateSinglePathWithURLs(t *testing.T) {
 
 	// for http sockets, the path must be empty
 	str = "http://localhost/index.html"
-	assert.NoError(validator.Validate(str))
 	value = validator.Value(str)
-	assert.NoError(value.Error)
-	assert.Len(value.values, 0)
+	assert.Error(value.Error)
+	assert.Len(value.values, 1)
 	assert.False(value.HasNext())
-	assert.Equal("--defaultValue--", value.String("--defaultValue--"))
+	assert.Equal(str, value.String("--defaultValue--"))
 
 	// for unix sockets, the hostname must be empty
 	str = "unix://var/run/foo.sock"
 	assert.Error(validator.Validate(str))
 	value = validator.Value(str)
 	assert.Error(value.Error)
-	assert.Len(value.values, 0)
+	assert.Len(value.values, 1)
+	assert.Equal(str, value.String("--defaultValue--"))
+}
+
+// TestValidateMultiPathWithoutURLs -- multiple file paths (no URL parsing)
+func TestValidateMultiPathWithoutURLs(t *testing.T) {
+	var assert = assert.New(t)
+	var validator = PathValidator{
+		AllowMultiple: true,
+	}
+
+	var value = validator.Value("")
+	assert.NoError(value.Error)
+	assert.Empty(value.values)
+	assert.False(value.HasNext())
 	assert.Equal("--defaultValue--", value.String("--defaultValue--"))
+
+	// single valid path
+	var str = "/var/run/ondevice.pid"
+	value = validator.Value(str)
+	assert.NoError(value.Error)
+	assert.Len(value.values, 1)
+	assert.False(value.HasNext())
+	assert.Equal(str, value.String("--defaultValue--"))
+
+	// single URL -> no error but won't be able to read/write the files
+	str = "http://localhost:8080/"
+	value = validator.Value(str)
+	assert.NoError(value.Error)
+	assert.Len(value.values, 1)
+	assert.False(value.HasNext())
+	assert.Equal(str, value.String("--defaultValue--"))
+
+	// valid JSON string array (since we don't do URL parsing, this won't cause errors)
+	str = "[\"~/.config/ondevice/ondevice.sock\", \"/var/run/ondevice.sock\", \"http://localhost:8080\"]"
+	value = validator.Value(str)
+	assert.NoError(value.Error)
+	assert.Len(value.values, 3)
+	assert.True(value.HasNext())
+	assert.Equal("~/.config/ondevice/ondevice.sock", value.String("--defaultValue--"))
+	value = value.Next()
+	assert.True(value.HasNext())
+	assert.Equal("/var/run/ondevice.sock", value.String("--defaultValue--"))
+	value = value.Next()
+	assert.False(value.HasNext())
+	assert.Equal("http://localhost:8080", value.String("--defaultValue--"))
+
+	// valid single-item JSON array
+	str = "[\"https://ondevice.io/index.html\"]"
+	value = validator.Value(str)
+	assert.NoError(value.Error)
+	assert.Len(value.values, 1)
+	assert.False(value.HasNext())
+	assert.Equal("https://ondevice.io/index.html", value.String("--defaultValue--"))
+
+	// invalid json -> should be interpreted as-is (since it starts with a '[', we'll issue a warning though)
+	str = "[\"https://ondevice.io/index.html\""
+	value = validator.Value(str)
+	assert.NoError(value.Error)
+	assert.Len(value.values, 1)
+	assert.False(value.HasNext())
+	assert.Equal(str, value.String("--defaultValue--"))
+
+	// valid JSON, but not a list -> should be interpreted as-is
+	str = "\"https://ondevice.io/index.html\""
+	value = validator.Value(str)
+	assert.NoError(value.Error)
+	assert.Len(value.values, 1)
+	assert.False(value.HasNext())
+	assert.Equal(str, value.String("--defaultValue--"))
+
+	// valid JSON list, but not of strings -> should be interpreted as-is (json.Unmarshal() should fail with a warning)
+	str = "[\"https://ondevice.io/index.html\", 1,2,3]"
+	value = validator.Value(str)
+	assert.NoError(value.Error)
+	assert.Len(value.values, 1)
+	assert.False(value.HasNext())
+	assert.Equal(str, value.String("--defaultValue--"))
 }
 
 // TestParseURL -- tests some edge cases of URL parsing with url.Parse()

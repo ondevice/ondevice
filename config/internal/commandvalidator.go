09c9671290ee3998a5e22277933ef18f0b043980
Manuel Reithuber <git@manuel.reithuber.net>
2020-07-29 23:38:29 +0200

    updated CommandValidator to be more strict with validating JSON (any string starting with '[' will now be treated as JSON array)

--- config/internal/commandvalidator.go
+++ config/internal/commandvalidator.go
@@ -2,42 +2,49 @@ package internal
 
 import (
 	"encoding/json"
+	"fmt"
 	"strings"
-
-	"github.com/sirupsen/logrus"
 )
 
-// CommandValidator -- constructs a config.Value from a string
+// CommandValidator -- parses and validates Command values
+//
+// There are two ways to specify commands
+// - literal string path
+// - JSON arrays (any string starting with the character '[' will be treated as such)
+//   invalid JSON strings will cause validation errors
+//
+// In the first case, the resulting Value will simply be an array with one single item: the string itself
+//
+// If you want ondevice to call the command in question with predefined arguments, use the JSON variant,
+// e.g: `ssh -C` (compressing data) should be declared as `["ssh", "-C"]`.
+//
+// to use a command starting with '[', wrap it inside a JSON array: `["["]`
 type CommandValidator struct{}
 
-// Validate -- always returns nil
+// Validate -- returns any errors found while parsing this Command Value
 func (v CommandValidator) Validate(value string) error {
 	return v.Value(value).Error
 }
 
 // Value -- parses the command config string into a config.Value
 //
-// if the string you pass to .Value() is a valid JSON string array, it'll be parsed and split into its components.
-// otherwise it'll
+// if the string you pass to .Value() starts with '[', it will be parsed as JSON.
+//
+// Otherwise the whole string will be put inside the first (and only) array element
 func (v CommandValidator) Value(raw string) Value {
-	var args []string
-
 	if len(raw) == 0 {
 		return Value{} // empty value -> use default
 	}
 
-	if err := json.Unmarshal([]byte(raw), &args); err == nil {
-		// valid JSON list - use as is
-		return Value{
-			values: args,
+	var rc Value
+	if strings.HasPrefix(raw, "[") {
+		// the value starts with '[', assume it is JSON
+		if err := json.Unmarshal([]byte(raw), &rc.values); err != nil {
+			rc.Error = fmt.Errorf("failed to parse JSON command: %s", err.Error())
 		}
-	} else if strings.HasPrefix(raw, "[") {
-		logrus.WithField("cmd", raw).WithError(err).Error("your command looks like it's in JSON format but contains errors")
-		return Value{Error: err}
+	} else {
+		// default behaviour: put it into the first slice
+		rc.values = []string{raw}
 	}
-
-	// default behaviour: put it into the first slice
-	return Value{
-		values: []string{raw},
-	}
+	return rc
 }
